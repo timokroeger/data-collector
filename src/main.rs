4087eb8156c90ea9a63f6f9c126f40f97d2d8ff6
Timo Kröger <timokroeger93@gmail.com>
2019-03-08 10:31:45 +0100

    Newtype for points and improve config parsing
    Let serde check if id and register fields exist while parsing the config file instead of unwrapping when a connection is already established

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use log::error;
 use serde::Deserialize;
 use tokio_modbus::prelude::*;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct Config {
     modbus_hostname: String,
     scan_interval_sec: u64,
@@ -16,7 +16,7 @@ struct Config {
     measurements: HashMap<String, Vec<ConfigMeasurement>>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 struct ConfigInfluxDb {
     hostname: String,
     database: String,
@@ -24,15 +24,38 @@ struct ConfigInfluxDb {
     password: Option<String>,
 }
 
-type ConfigMeasurement = HashMap<String, toml::Value>;
+#[derive(Deserialize)]
+struct ConfigMeasurement {
+    id: u8,
+    register: u16,
+
+    #[serde(flatten)]
+    tags: HashMap<String, toml::Value>,
+}
+
+struct Point(influxdb::Point);
+
+impl Point {
+    fn new(measurement: &str, value: u16, config: &ConfigMeasurement) -> Point {
+        let mut p = influxdb::Point::new(&measurement);
+        p.add_field("value", influxdb::Value::Integer(i64::from(value)));
+        p.add_tag("id", influxdb::Value::String(config.id.to_string()));
+        p.add_tag(
+            "register",
+            influxdb::Value::String(config.register.to_string()),
+        );
+        for (tag_name, tag_value) in &config.tags {
+            p.add_tag(tag_name, influxdb::Value::String(tag_value.to_string()));
+        }
+        Point(p)
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     env_logger::init();
 
     let config_str = fs::read_to_string("datacollector.toml")?;
     let config: Config = toml::from_str(&config_str)?;
-
-    // TODO: connect to modbus server
 
     let db = influxdb::Client::new(config.influxdb.hostname, config.influxdb.database);
     let db = match (config.influxdb.username, config.influxdb.password) {
@@ -53,16 +76,12 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
         };
 
         'connection: loop {
-            let mut points = Vec::new();
-            for (measurement_name, measurements) in &config.measurements {
-                for m in measurements {
-                    let mut p = influxdb::Point::new(&measurement_name);
+            let mut influx_points = Vec::new();
 
-                    let id = m["id"].as_integer().unwrap() as u8;
-                    ctx.set_slave(Slave(id));
-
-                    let register = m["register"].as_integer().unwrap() as u16;
-                    let value = match ctx.read_input_registers(register, 1) {
+            for (name, points) in &config.measurements {
+                for p in points {
+                    ctx.set_slave(Slave(p.id));
+                    let value = match ctx.read_input_registers(p.register, 1) {
                         Ok(v) => v[0],
                         Err(e) => {
                             error!("Modbus: {}, reconnecting...", e);
@@ -70,17 +89,12 @@ fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
                         }
                     };
 
-                    p.add_field("value", influxdb::Value::Integer(i64::from(value)));
-                    for (tag_name, tag_value) in m {
-                        p.add_tag(tag_name, influxdb::Value::String(tag_value.to_string()));
-                    }
-
-                    points.push(p);
+                    influx_points.push(Point::new(name, value, p).0);
                 }
             }
 
             db.write_points(
-                influxdb::Points::create_new(points),
+                influxdb::Points::create_new(influx_points),
                 Some(influxdb::Precision::Seconds),
                 None,
             )
