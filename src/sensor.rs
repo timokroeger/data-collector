b4e19d34c0eef5f47add9bc954dd3e9845e5c578
Timo Kröger <timokroeger93@gmail.com>
2019-03-15 14:48:28 +0100

    Move read call grouping to register map struct

--- src/sensor.rs
+++ src/sensor.rs
@@ -7,28 +7,44 @@ use log::warn;
 use modbus::{Client, Error as ModbusError};
 
 /// A Register map that has the register address as key and measurement name as value.
-pub struct RegisterMap(BTreeMap<u16, String>);
+pub struct RegisterMap {
+    registers: BTreeMap<u16, String>,
 
-impl RegisterMap {
-    pub fn new(register_names: BTreeMap<u16, String>) -> Self {
-        Self(register_names)
-    }
-
-    // Group consecutive registers into one request
-    fn merged_reads(&self) -> Vec<ReadRegistersParams> {
-        let mut params: Vec<ReadRegistersParams> = Vec::new();
-        for &r in self.0.keys() {
-            match params.last_mut() {
-                Some(ref mut p) if r == p.0 + p.1 => p.1 += 1,
-                _ => params.push(ReadRegistersParams(r, 1)),
-            }
-        }
-        params
-    }
+    /// A group contains the starting address and number of registers required by read call.
+    read_groups: Vec<(u16, u16)>,
 }
 
-#[derive(PartialEq)]
-struct ReadRegistersParams(u16, u16);
+impl RegisterMap {
+    pub fn new(registers: BTreeMap<u16, String>) -> Self {
+        // A BTreeMap garantues sorted keys, addresses will be in order.
+        let addrs = registers.keys().cloned().collect();
+        Self {
+            registers,
+            read_groups: RegisterMap::group_registers(addrs),
+        }
+    }
+
+    /// Groups consecutive registers into one request.
+    /// Assumes the addresses to be sorted.
+    fn group_registers(addrs: Vec<u16>) -> Vec<(u16, u16)> {
+        let mut groups: Vec<(u16, u16)> = Vec::new();
+        for addr in addrs {
+            match groups.last_mut() {
+                Some(ref mut p) if addr == p.0 + p.1 => p.1 += 1,
+                _ => groups.push((addr, 1)),
+            }
+        }
+        groups
+    }
+
+    pub fn read_groups(&self) -> &[(u16, u16)] {
+        &self.read_groups
+    }
+
+    pub fn get_name(&self, addr: u16) -> &str {
+        &self.registers[&addr]
+    }
+}
 
 fn new_influxdb_point(
     measurement: &str,
@@ -50,9 +66,6 @@ pub struct Sensor<'a> {
     group: &'a str,
     registers: &'a RegisterMap,
     tags: Vec<(String, String)>,
-
-    // TODO: Move this field to RegisterMap
-    read_reg_calls: Vec<ReadRegistersParams>,
 }
 
 impl<'a> Sensor<'a> {
@@ -67,7 +80,6 @@ impl<'a> Sensor<'a> {
             group,
             registers,
             tags,
-            read_reg_calls: registers.merged_reads(),
         }
     }
 
@@ -75,7 +87,7 @@ impl<'a> Sensor<'a> {
         let mut result = HashMap::new();
 
         mb.set_slave(self.id);
-        for param in &self.read_reg_calls {
+        for param in self.registers.read_groups() {
             match mb.read_input_registers(param.0, param.1) {
                 Ok(values) => {
                     result.extend(&mut (param.0..param.0 + param.1).zip(values));
@@ -93,16 +105,17 @@ impl<'a> Sensor<'a> {
     pub fn get_points(&self, register_values: &HashMap<u16, u16>) -> Vec<Point> {
         let mut points = Vec::new();
 
-        for (r, v) in register_values {
+        for (&reg_addr, &value) in register_values {
             let mut tags = Vec::new();
             tags.push(("group".to_string(), self.group.to_string()));
             tags.push(("id".to_string(), self.id.to_string()));
-            tags.push(("register".to_string(), r.to_string()));
+            tags.push(("register".to_string(), reg_addr.to_string()));
             tags.append(&mut self.tags.clone());
+
             points.push(new_influxdb_point(
-                &self.registers.0[&r],
+                &self.registers.get_name(reg_addr),
                 SystemTime::now(),
-                *v,
+                value,
                 &tags,
             ));
         }
